8446aa9345c1a600d0a9aeab64904bea58d59af0
Renuka Choudhary <renurunrun@gmail.com>
2022-11-10 23:41:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React,{useState , useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [query,setQuery] = useState("Paneer")
+  const [data,setData] = useState([]);
+  const [isClicked, setIsClicked] = useState(false)
+  useEffect(()=>{
+    fetch(`https://api.edamam.com/api/recipes/v2?q=${query}&app_id=bdd0b193&app_key=f9c8c35956bfd7d44ef4f2affe291ae4&type=public`).then(
+      (Response) => Response.json()
+    ).then(
+      (data) => {
+        const arrayData = data.hits
+        setData(arrayData)
+        })
+        console.log(data)
+        // eslint-disable-next-line 
+      },[isClicked]
+    )
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       {query}
+       <input onChange={(e)=>setQuery(e.target.value)} type="text"></input>
+     <button onClick={()=>{setIsClicked(true)}}>Fetch</button>
+      {
+        data.map((item,i)=>{
+          return <div key={i}>
+          <p>{item.recipe.label} </p> <br/>
+           <img src={item.recipe.image}></img>
+          </div>
+        })
+      }
     </div>
   );
 }
